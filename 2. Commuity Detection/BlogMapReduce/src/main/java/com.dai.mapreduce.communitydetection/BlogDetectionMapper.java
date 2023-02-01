f0a16922eb11fa036b7ec21afd842b77e0d37319
daiyayuan <836919594@qq.com>
2023-02-01 21:45:53 +0800

    java mapper commit

--- 2. Commuity Detection/BlogMapReduce/src/main/java/com.dai.mapreduce.communitydetection/BlogDetectionMapper.java
+++ 2. Commuity Detection/BlogMapReduce/src/main/java/com.dai.mapreduce.communitydetection/BlogDetectionMapper.java
@@ -1,6 +1,5 @@
 package com.dai.mapreduce.communitydetection;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -8,14 +7,36 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 /**
+ * Task a: 寻找两篇博客的共同关注
+ * Input: _relation
  * KEYIN: map阶段的输入的key的类型，每次读取的偏移量 LongWritable
- * VALUEIN: map阶段输入的value
+ * VALUEIN: map阶段输入的value Text: 读取一整行
  * KEYOUT: map阶段输出的key
  * VALUEOUT: may阶段输出的value
  */
-public class BlogDetectionMapper extends Mapper<LongWritable, Text, Text, Text> {
+public class BlogDetectionMapper extends Mapper<LongWritable, Text, LongWritable, LongWritable> {
+    // 该map输出的key
+    private LongWritable outKey = new LongWritable();
+    // 该map输出的value
+    private LongWritable outValue = new LongWritable();
+    /**
+     * map 阶段, map每行都会被调用一次
+     * @param key 输入时的key
+     * @param value 输入时的value
+     * @param context
+     * @throws IOException
+     * @throws InterruptedException
+     */
     @Override
-    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
-        super.map(key, value, context);
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, LongWritable, LongWritable>.Context context) throws IOException, InterruptedException {
+        // 获取一行
+        String line = value.toString();
+        // 用空格将一行中的两个数据进行切割 [A，B] 表示A follow B
+        String[] words = line.split(" ");
+        // 关注者作为key
+        outKey.set(Long.parseLong(words[0]));
+        // 被关注者作为value
+        outValue.set(Long.parseLong(words[1]));
+        context.write(outKey, outValue);
     }
 }
