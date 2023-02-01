908f1d47549fb016481bdca09f46929607951a55
daiyayuan <836919594@qq.com>
2023-02-01 21:57:20 +0800

    java reducer commit

--- 2. Commuity Detection/BlogMapReduce/src/main/java/com.dai.mapreduce.communitydetection/BlogDetectionReducer.java
+++ 2. Commuity Detection/BlogMapReduce/src/main/java/com.dai.mapreduce.communitydetection/BlogDetectionReducer.java
@@ -1,4 +1,19 @@
 package com.dai.mapreduce.communitydetection;
 
-public class BlogDetectionReducer {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * reducer 层接受 mapper 的 OUT
+ * KEYIN 和 VALUEIN 与 mapper的 OUT 保持一致
+ * IN: 每对图关系 A->B
+ * output 格式:s A:B, {C,E}, 2
+ */
+public class BlogDetectionReducer extends Reducer<LongWritable, LongWritable, LongWritable, LongWritable> {
+    @Override
+    protected void reduce(LongWritable key, Iterable<LongWritable> values, Reducer<LongWritable, LongWritable, LongWritable, LongWritable>.Context context) throws IOException, InterruptedException {
+
+    }
 }
