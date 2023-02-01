1e3a51ad2723415c0e4596d06ee1bd5f6085397c
daiyayuan <836919594@qq.com>
2023-02-01 21:46:25 +0800

    java mapper commit

--- 2. Commuity Detection/BlogMapReduce/src/main/java/com.dai.mapreduce.communitydetection/BlogDetectionMapper.java
+++ 2. Commuity Detection/BlogMapReduce/src/main/java/com.dai.mapreduce.communitydetection/BlogDetectionMapper.java
@@ -3,7 +3,6 @@ package com.dai.mapreduce.communitydetection;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-
 import java.io.IOException;
 
 /**
