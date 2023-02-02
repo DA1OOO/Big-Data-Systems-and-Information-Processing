35970b487084a9935e474185a2e8a4832f750a4d
DA1YAYUAN <836919594@qq.com>
2023-02-02 11:27:33 +0800

    reducer / driver commit

--- 2. Commuity Detection/BlogMapReduce/src/main/java/com.dai.mapreduce.communitydetection/BlogDetectionDriver.java
+++ 2. Commuity Detection/BlogMapReduce/src/main/java/com.dai.mapreduce.communitydetection/BlogDetectionDriver.java
@@ -1,4 +1,36 @@
 package com.dai.mapreduce.communitydetection;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
 public class BlogDetectionDriver {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        // 1. 获取job
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+        // 2. 设置jar包路径
+        job.setJarByClass(BlogDetectionDriver.class);
+        // 3. 关联 mapper 和 reducer
+        job.setMapperClass(BlogDetectionMapper.class);
+        job.setReducerClass(BlogDetectionReducer.class);
+        // 4. 设置 mapper 输出的KV类型
+        job.setMapOutputKeyClass(LongWritable.class);
+        job.setMapOutputValueClass(LongWritable.class);
+        // 5. 设置最终输出的KV类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        // 6. 设置输入输出路径
+        FileInputFormat.setInputPaths(job, new Path("C:\\Users\\Administrator\\Desktop\\Big-Data-Systems-and-Information-Processing\\2. Commuity Detection\\medium\\medium"));
+        FileOutputFormat.setOutputPath(job, new Path("C:\\Users\\Administrator\\Desktop\\Big-Data-Systems-and-Information-Processing\\2. Commuity Detection\\output"));
+        // 7. 提交job
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+    }
 }
--- 2. Commuity Detection/BlogMapReduce/src/main/java/com.dai.mapreduce.communitydetection/BlogDetectionReducer.java
+++ 2. Commuity Detection/BlogMapReduce/src/main/java/com.dai.mapreduce.communitydetection/BlogDetectionReducer.java
@@ -1,9 +1,12 @@
 package com.dai.mapreduce.communitydetection;
 
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * reducer 层接受 mapper 的 OUT
@@ -11,9 +14,25 @@ import java.io.IOException;
  * IN: 每对图关系 A->B
  * output 格式:s A:B, {C,E}, 2
  */
-public class BlogDetectionReducer extends Reducer<LongWritable, LongWritable, LongWritable, LongWritable> {
-    @Override
-    protected void reduce(LongWritable key, Iterable<LongWritable> values, Reducer<LongWritable, LongWritable, LongWritable, LongWritable>.Context context) throws IOException, InterruptedException {
+public class BlogDetectionReducer extends Reducer<LongWritable, LongWritable, Text, Text> {
+    private Text outKey = new Text();
+    private Text outValue = new Text();
 
+    /**
+     * 重写reduce方法，每个key都会运行一次reduce方法
+     * @param key 输入的key
+     * @param values 按key合并后的value的组合
+     * @param context
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Override
+    protected void reduce(LongWritable key, Iterable<LongWritable> values, Reducer<LongWritable, LongWritable, Text, Text>.Context context) throws IOException, InterruptedException {
+        List<LongWritable> list = new ArrayList<LongWritable>();
+        for (LongWritable value : values) {
+            list.add(value);
+        }
+        // 写出输出数据到上下文
+        context.write(outKey, outValue);
     }
 }
