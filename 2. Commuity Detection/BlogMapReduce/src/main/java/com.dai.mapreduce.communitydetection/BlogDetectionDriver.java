65fe7c6b9b64a558f904176235d4cd226519179b
DA1YAYUAN <836919594@qq.com>
2023-02-02 17:06:29 +0800

    支持自定义输入输出路径

--- 2. Commuity Detection/BlogMapReduce/src/main/java/com.dai.mapreduce.communitydetection/BlogDetectionDriver.java
+++ 2. Commuity Detection/BlogMapReduce/src/main/java/com.dai.mapreduce.communitydetection/BlogDetectionDriver.java
@@ -15,20 +15,28 @@ public class BlogDetectionDriver {
         // 1. 获取job
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf);
+
         // 2. 设置jar包路径
         job.setJarByClass(BlogDetectionDriver.class);
+
         // 3. 关联 mapper 和 reducer
         job.setMapperClass(BlogDetectionMapper.class);
         job.setReducerClass(BlogDetectionReducer.class);
+
         // 4. 设置 mapper 输出的KV类型
         job.setMapOutputKeyClass(LongWritable.class);
         job.setMapOutputValueClass(LongWritable.class);
+
         // 5. 设置最终输出的KV类型
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
+
         // 6. 设置输入输出路径
         FileInputFormat.setInputPaths(job, new Path("C:\\Users\\Administrator\\Desktop\\Big-Data-Systems-and-Information-Processing\\2. Commuity Detection\\input\\small\\small_relation"));
+        //FileInputFormat.setInputPaths(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path("C:\\Users\\Administrator\\Desktop\\Big-Data-Systems-and-Information-Processing\\2. Commuity Detection\\output"));
+        //FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
         // 7. 提交job
         boolean result = job.waitForCompletion(true);
         System.exit(result ? 0 : 1);
